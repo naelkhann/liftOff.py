6ea5414d7d4db0b4e593f89c6ed0a03da03c07fd
Nael Khan <naelkhann@gmail.com>
2016-02-18 20:43:04 -0500

    Scrapes image function finished

--- liftOff.py
+++ liftOff.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-#LiftOff - Pull newest images from interfacelift.com and downlod them into my personal Wallpaper folder
+#LiftOff - Pull newest images from interfacelift.com and download them into my personal Wallpaper folder
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -12,12 +12,12 @@ driver.get("http://interfacelift.com/wallpaper/downloads/date/any")
 assert "InterfaceLIFT" in driver.title, "InterfaceLIFT title doesn't match"
 
 dropdown_click = driver.find_element_by_name('resolution').click()
-resolution = driver.find_element_by_xpath("//select/optgroup[@label='Widescreen 16:10']/option[@id='res_1680x1050_1']")
+resolution = driver.find_element_by_xpath("//select/optgroup[@label='Widescreen 16:10']/option[@id='res_1680x1050_1']").click()
 
-resolution.click()
-resolution.click()
+download = driver.find_element_by_xpath("//div[@id='download_4028']/a")
 
-
+download.click()
+download.click()
 # driver.implicitly_wait(1)
 
 # pull_url = driver.current_url
