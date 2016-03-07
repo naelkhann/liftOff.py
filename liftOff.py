fe1fced5f7b96be4936dfaeaaf0f28cea5686ab4
Nael Khan <naelkhann@gmail.com>
2016-03-07 01:46:46 -0500

    Added exception handling to liftOff.py

--- liftOff.py
+++ liftOff.py
@@ -4,33 +4,43 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import sys, requests, os, imageio
+from selenium.common.exceptions import NoSuchElementException
+import sys, os, urllib
 
 if len(sys.argv) > 1:
 	i = sys.argv[1]
 else:
-	i = 4027
+	i = 4039
 
+index_num = str(i)
 driver = webdriver.Firefox()
 driver.get("http://interfacelift.com/wallpaper/downloads/date/any")
 assert "InterfaceLIFT" in driver.title, "InterfaceLIFT title doesn't match"
 
-dropdown_click = driver.find_element_by_name('resolution').click()
-resolution = driver.find_element_by_xpath("//select/optgroup[@label='Widescreen 16:10']/option[@id='res_1440x900_1']").click()
+try:
+	dropdown_click = driver.find_element_by_name('resolution').click()
+	resolution = driver.find_element_by_xpath("//select/optgroup[@label='Widescreen 16:10']/option[@id='res_1440x900_1']").click()
 
-item_to_dl = "//div[@id='download_" + str(i) + "']/a"
-download = driver.find_element_by_xpath(item_to_dl)
+	item_to_dl = "//div[@id='download_" + index_num + "']/a"
+	download = driver.find_element_by_xpath(item_to_dl)
 
-download.click()
+	download.click()
 
-download.click()
+	download.click()
 
-pull_url = driver.current_url
-#file_url_from_pull = pull_url[44:]
+	pull_url = driver.current_url
+	#file_url_from_pull = pull_url[44:]
 
-imgg = imageio.imread(pull_url)
-save_imgg = imageio.imwrite('saved.png', imgg)
-driver.close()
+	file_name = index_num + ".jpg"
+	urllib.urlretrieve(pull_url, file_name)
+	driver.close()
+	
+except NoSuchElementException:
+	print "Could not find", index_num, "on site. Might be too soon, check back when new file is uploaded"
+	driver.close()
+	exit()
+
+
 
 
 
